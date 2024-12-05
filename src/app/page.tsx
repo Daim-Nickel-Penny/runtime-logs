9be3417feadfd3a8cccdbcf6aac0b7e85cf0f9a0
Daim-Nickel-Penny <1804449@kiit.ac.in>
2024-12-05 16:35:35 +0530

    Added prettier tailwind plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div className="m-20 flex flex-col justify-center border border-slate-100/15 p-20">
+      <div className="text-center font-sans text-5xl font-bold leading-tight tracking-tighter">
+        Runtime logs to see event <br /> sequences for API requests
+      </div>
+    </div>
+  );
 }
